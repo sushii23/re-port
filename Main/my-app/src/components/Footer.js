b9ec6e96fe1b5bd83ba0ff93dfb3ea45c4885137
Adrian Deon <sushdawg@Adrians-MBP.frontiernet.net>
2023-01-30 02:34:44 -0600

    added footer comp

---
+++ Main/my-app/src/components/Footer.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Footer = () => {
+    return (
+        <footer>
+            <a href="#">GitHub</a>
+            <a href="#">LinkedIn</a>
+            <a href="#">Stack Overflow</a>
+        </footer>
+    );
+};
+
+export default Footer;
