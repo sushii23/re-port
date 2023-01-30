c194777615712274a264e9a7183621c0cbedc370
Adrian Deon <sushdawg@Adrians-MBP.frontiernet.net>
2023-01-30 02:25:09 -0600

    added nav comp

---
+++ Main/my-app/src/components/Nav.js
@@ -0,0 +1,23 @@
+import React, { useState} from "react";
+
+const Nav = () => {
+    const [selected, setSelected] = useState("About Me");
+    const selections = ["About Me", "Portfolio", "Contact", "Resume"];
+
+    return (
+        <nav>
+            {selections.map(section => ( 
+              <a
+              key={section}
+              href= "#"
+              className={selected === section ? "selected" : ""}
+              onClick={() => setSelected(section)}
+              >
+                {section}
+              </a>   
+            ))}
+        </nav>
+    );
+};
+
+export default Nav;
