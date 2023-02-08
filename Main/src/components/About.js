ac9022bab3ff3c8efe4e08fca6b4df04beec1e91
Adrian Deon <sushdawg@Adrians-MBP.frontiernet.net>
2023-02-08 03:40:27 -0600

    debug

---
+++ Main/src/components/About.js
@@ -0,0 +1,25 @@
+import React from 'react';
+
+const About = () => {
+  return (
+    <>
+      <header>
+        <h1>Adrian Williams</h1>
+        
+      </header>
+      <main>
+        <section>
+          <img src="avad.jpg" alt="Avatar" />
+          <p>Short bio about the developer</p>
+        </section>
+      </main>
+      {/* <footer>
+        <a href="#">GitHub</a>
+        <a href="#">LinkedIn</a>
+        <a href="#">Stack Overflow</a>
+      </footer> */}
+    </>
+  );
+};
+
+export default About;
